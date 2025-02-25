7273ec1a7c8b02a7032f0f34f28af65907eb1911
dasmikko <dasmikko@gmail.com>
2025-02-25 12:14:33 +0100

    Add support for showing shell command output without -v flag

--- src/shell.ts
+++ src/shell.ts
@@ -1,17 +1,17 @@
 import {$} from "zx";
 import {globalArgs} from "./global.ts";
+import {$ as bunShell} from "bun";
 
 /**
  * Run a shell command and return the exit code and the output
  * @param shellCommand
  * @returns {Promise<{exitCode: number, stdout: string, stderr: string}>}
  */
-export async function runShellCommand(shellCommand: string, vars: object = {}): Promise<{ exitCode: number, stdout: string, stderr: string }> {
+export async function runShellCommand(shellCommand: string, vars: object = {}, showOutput: boolean = false): Promise<{ exitCode: number, stdout: string, stderr: string }> {
     // Prepare vars by making keys uppercase
     const varsUppercase = Object.fromEntries(
         Object.entries(vars).map(([key, value]) => [key.toUpperCase(), value])
     )
-
 
     // Setup the shell function
     const $$ = $({
@@ -27,7 +27,31 @@ export async function runShellCommand(shellCommand: string, vars: object = {}): 
     // Run the shell command
     const { stdout, stderr, exitCode } = await $$`bash -c ${shellCommand}`
 
+    if (showOutput) {
+        console.log(stdout.toString().trim())
+    }
+
     // Return the exit code and the output
+    return {
+        exitCode : exitCode === null ? 0 : exitCode,
+        stdout: stdout.toString(),
+        stderr: stderr.toString(),
+    }
+}
+
+
+export async function runZxScript (scriptPath: string, vars: object = {}): Promise<{ exitCode: number, stdout: string, stderr: string }> {
+    // Prepare vars by making keys uppercase
+    const varsUppercase = Object.fromEntries(
+        Object.entries(vars).map(([key, value]) => [key.toUpperCase(), value])
+    )
+
+    // Run the shell command
+    const { stdout, stderr, exitCode } = await bunShell`${scriptPath}`.env({
+        ...process.env,
+        ...varsUppercase
+    })
+
     return {
         exitCode : exitCode === null ? 0 : exitCode,
         stdout: stdout.toString(),
