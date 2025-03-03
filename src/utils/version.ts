c110afd9bc8495c687000e8f309ad22ae1df9ed4
dasmikko <dasmikko@gmail.com>
2025-03-03 11:22:09 +0100

    add more logging

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,15 +1,25 @@
 import semver, {SemVer} from "semver";
+import {globalArgs} from "../global.ts";
+import {consola} from "consola";
 
 /**
  * Parse a version string into a SemVer object. It will try to coerce the version string into a valid SemVer object.
  * @param versionString
  */
-export function parseVersion(versionString: string): SemVer {
-    const packageVersion = semver.coerce(versionString);
+export function parseVersion(versionString: string): SemVer | null | string {
+    try {
+        const packageVersion = semver.coerce(versionString);
 
-    if (packageVersion === null) {
-        throw new Error(`Version could not be parsed from ${versionString}`);
+        console.log(packageVersion)
+        if (packageVersion === null) {
+            if (globalArgs.verboseMode) consola.error(`Version could not be parsed from "${String(versionString)}"`)
+
+            return 'Unknown'
+        }
+
+        return packageVersion.version
+    } catch (e) {
+        consola.error(`Version could not be parsed from "${String(versionString)}"`)
+        return 'Unknown'
     }
-
-    return packageVersion
 }
